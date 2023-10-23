494457c21e9f5a7a0563919cd08497acd7863eb6
mlev29 <ml.290620056@gmail.com>
2023-10-22 22:20:04 -0400

    reformat project

---
+++ src/LightSync.cpp
@@ -0,0 +1,14 @@
+﻿// LightSync.cpp : Defines the entry point for the application.
+//
+
+#include "../include/LightSync.h"
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cout << "Hello CMake." << endl;
+	cin >> s;
+	return 0;
+}
