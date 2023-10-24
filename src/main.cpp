539b40ae1d47691ae56c78b05eab74badbc7e277
mlev29 <ml.290620056@gmail.com>
2023-10-23 22:05:24 -0400

    specify header directory & change file name Lightsync.cpp to main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+﻿// LightSync.cpp : Defines the entry point for the application.
+//
+
+#include "LightSync.h"
+
+using namespace std;
+
+int main()
+{
+    //inputColor();
+    // Alienware
+    //alienwareStaticColor();
+
+    // Logitech
+    // TODO: Coming soon...
+
+    std::cout << "\033[1;32mProgram execution success\033[0m\n";
+	return 0;
+}
